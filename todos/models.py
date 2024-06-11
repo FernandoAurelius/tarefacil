1e7536ef23f8c3f51481389797b7e1d16ba3ec91
Miguel Fernando Flores <miguelfernandoaurelius@gmail.com>
2024-06-11 15:04:11 -0300

    feat: complete todo page, TodoCompleteView, url 'todo_complete' | fix: bug: creating todo with passed deadline, add form_valid method in TodoCreateView

--- todos/models.py
+++ todos/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from datetime import date
+
 
 class Todo(models.Model):
     title = models.CharField(
@@ -8,3 +10,11 @@ class Todo(models.Model):
     created_at = models.DateField(auto_now_add=True, null=False, blank=False)
     deadline = models.DateField(verbose_name="Prazo final", null=False, blank=False)
     finished_at = models.DateField(null=True)
+
+    class Meta:
+        ordering = ["deadline"]
+
+    def mark_has_complete(self):
+        if not self.finished_at:
+            self.finished_at = date.today()
+            self.save()
--- todos/views.py
+++ todos/views.py
@@ -1,6 +1,14 @@
-from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView, View
 
 from django.urls import reverse_lazy
+
+from django.http import HttpRequest
+
+from django.forms import forms
+
+from django.shortcuts import get_object_or_404, redirect
+
+from django.utils import timezone
 
 from todos.models import Todo
 
@@ -14,6 +22,15 @@ class TodoCreateView(CreateView):
     fields = ["title", "deadline"]
     success_url = reverse_lazy("todo_list")
 
+    def form_valid(self, form):
+        deadline = form.cleaned_data.get("deadline")
+        if deadline < timezone.now().date():
+            today = timezone.now().date()
+            today = today.strftime("%d/%m/%Y")
+            form.add_error("deadline", f"O prazo de entrega precisa ser igual ou superior à data de hoje ({today}).")
+            return self.form_invalid(form)
+        return super().form_valid(form)
+
 
 class TodoUpdateView(UpdateView):
     model = Todo
@@ -24,3 +41,10 @@ class TodoUpdateView(UpdateView):
 class TodoDeleteView(DeleteView):
     model = Todo
     success_url = reverse_lazy("todo_list")
+
+
+class TodoCompleteView(View):
+    def get(self, request, pk):
+        todo = get_object_or_404(Todo, pk=pk)
+        todo.mark_has_complete()
+        return redirect("todo_list")
