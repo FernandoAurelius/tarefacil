b484b47e1adaf35889c9566bbff0ca3efcab25ee
Miguel Fernando Flores <miguelfernandoaurelius@gmail.com>
2024-06-15 11:27:10 -0300

    feat|refactor: associate user with todo, create model OwnerModel, create view OwnerView, delete unused archives in app users

--- todos/models.py
+++ todos/models.py
@@ -2,8 +2,20 @@ from django.db import models
 
 from datetime import date
 
+from django.conf import settings
 
-class Todo(models.Model):
+
+class OwnedModel(models.Model):
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+
+    class Meta:
+        abstract = True
+
+
+class Todo(OwnedModel, models.Model):
     title = models.CharField(
         verbose_name="Título", max_length=100, null=False, blank=False
     )
--- todos/views.py
+++ todos/views.py
@@ -12,17 +12,37 @@ from django.utils import timezone
 
 from todos.models import Todo
 
-class TodoListView(LoginRequiredMixin, ListView):
+# Defini uma classe Mixin "OwnedMixin", que herda de "LoginRequiredMixin", e defini os métodos get_queryset e form_valid de maneira a não ter
+# que repetir o código em todas as outras CBVs
+class OwnedMixin(LoginRequiredMixin):
+    def get_queryset(self, *args, **kwargs):
+        return super().get_queryset(*args, **kwargs).filter(
+            owner=self.request.user
+        )
+    
+    def check_ownership(self, obj):
+        if obj.owner != self.request.user:
+            messages.error(self.request, "Você não tem permissão para modificar esta tarefa.")
+            return False
+        return True
+    
+    def set_owner(self, form):
+        form.instance.owner = self.request.user
+        return form
+
+
+class TodoListView(OwnedMixin, ListView):
     login_url = "accounts/login/"
     model = Todo
 
-class TodoCreateView(LoginRequiredMixin, CreateView):
+class TodoCreateView(OwnedMixin, CreateView):
     login_url = "accounts/login/"
     model = Todo
     fields = ["title", "deadline"]
     success_url = reverse_lazy("todo_list")
 
     def form_valid(self, form):
+        form = self.set_owner(form)
         deadline = form.cleaned_data.get("deadline")
         if deadline < timezone.now().date():
             today = timezone.now().date()
@@ -34,20 +54,30 @@ class TodoCreateView(LoginRequiredMixin, CreateView):
             return self.form_invalid(form)
         return super().form_valid(form)
 
-class TodoUpdateView(LoginRequiredMixin, UpdateView):
+class TodoUpdateView(OwnedMixin, UpdateView):
     login_url = "accounts/login/"
     model = Todo
     fields = ["title", "deadline"]
     success_url = reverse_lazy("todo_list")
 
-class TodoDeleteView(LoginRequiredMixin, DeleteView):
+    def form_valid(self, form):
+        form = self.set_owner(form)
+        return super().form_valid(form)
+
+class TodoDeleteView(OwnedMixin, DeleteView):
     login_url = "accounts/login/"
     model = Todo
     success_url = reverse_lazy("todo_list")
 
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        if not self.check_ownership(self.object):
+            return redirect("todo_list")
+        return super().post(request, *args, **kwargs)
+
 class TodoCompleteView(View):
     login_url = "accounts/login/"
     def get(self, request, pk):
-        todo = get_object_or_404(Todo, pk=pk)
+        todo = get_object_or_404(Todo, pk=pk, owner=request.user)
         todo.mark_has_complete()
         return redirect("todo_list")
